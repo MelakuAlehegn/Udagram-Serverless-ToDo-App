6df09879502d2e130cf57305689c4aa7331679e4
melakualehegn <melakualehegn34@gmail.com>
2022-07-28 19:58:45 +0300

    final comiit

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = 'tgdqyxa4i4';
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-wmckcgnj.us.auth0.com',
+  clientId: 'yYsOo7MnAv7j6GpaR2OaUk9mDM7sCaQM',
+  callbackUrl: 'http://localhost:3000/callback'
+};
